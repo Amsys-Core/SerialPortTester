f9b60935453b01efca4800c2d7cae7e27112f876
DrWoose <61382169+DrWoose@users.noreply.github.com>
2026-05-27 14:42:08 +0800

    feat: open bottle bottom and body serial port

--- SerialPortTester.go
+++ SerialPortTester.go
@@ -19,4 +19,37 @@ func main() {
 			fmt.Printf("Found port: %v\n", port)
 		}
 	}
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+
+	buff := make([]byte, 100)
+
+	bottleBodyPortName, err := serial.Open(setCOMPort(ports, "Bottle Body"), mode)
+	bottleBottomPortName, err := serial.Open(setCOMPort(ports, "Bottle Bottom"), mode)
+	controlPanelPortName, err := serial.Open(setCOMPort(ports, "Control Panel"), mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+func setCOMPort(ports []string, section string) string {
+	println("Please set the " + section + " com port")
+	var inputPortName string
+	_, err := fmt.Scanln(&inputPortName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, port := range ports {
+		if port == inputPortName {
+			fmt.Printf("Found port: %v\n", port)
+			return port
+		}
+	}
+	return ""
 }
